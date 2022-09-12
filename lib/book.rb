f949b072bf47438ee63b9190722be463e19ceeb4
tkyg <80114433+tkyg@users.noreply.github.com>
2022-09-12 17:21:37 -0400

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+# require 'pry'
+class Book
+  def initialize(title)
+    @title = title
+  end
+  attr_accessor :title, :author, :page_count, :genre
 
-
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+# binding pry
